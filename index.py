e12cf86f36b565f53be2926be4322c2cc77f9c8e
Albanos <albanostlabo0@gmail.com>
2025-06-20 09:41:40 +0200

    refactor: just changes  the logic for how the days and minutes works

--- index.py
+++ index.py
@@ -1,39 +1,60 @@
-def add_time(time, duration):
+def add_time(time, duration, date=''):
+    # Split start time
+    time_parts = time.split()
+    hour_min = time_parts[0].split(':')
+    am_pm = time_parts[1]
 
-    time_collen = time.replace(' ', '').find(':')
-    duration_collen = duration.find(':')
-    if time[time_collen + 4:] == 'PM':
-        format_collen = time.find('P') 
+    start_hour = int(hour_min[0])
+    start_minute = int(hour_min[1])
+
+    if am_pm == 'PM' and start_hour != 12:
+        start_hour += 12
+    elif am_pm == 'AM' and start_hour == 12:
+        start_hour = 0
+
+    dur_hour, dur_minute = map(int, duration.split(':'))
+
+    total_minutes = start_minute + dur_minute
+    extra_hour = total_minutes // 60
+    final_minutes = total_minutes % 60
+
+    total_hours = start_hour + dur_hour + extra_hour
+    days_later = total_hours // 24
+    final_hour_24 = total_hours % 24
+
+    if final_hour_24 == 0:
+        final_hour = 12
+        period = 'AM'
+    elif final_hour_24 < 12:
+        final_hour = final_hour_24
+        period = 'AM'
+    elif final_hour_24 == 12:
+        final_hour = 12
+        period = 'PM'
     else:
-        format_collen = time.find('A')
+        final_hour = final_hour_24 - 12
+        period = 'PM'
 
+    # Weekday calculation
+    week = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    final_day = ''
+    
+    if date:
+        index = week.index(date.capitalize())
+        final_day = week[(index + days_later) % 7]
 
-    left_time = time[:time_collen]
-    left_duration = duration[:duration_collen]
-    right_time = time[time_collen + 1:format_collen]
-    right_duration = duration[duration_collen + 1:]
+    if days_later == 1:
+        day_note = ' (next day)'
+    elif days_later > 1:
+        day_note = f' ({days_later} days later)'
+    else:
+        day_note = ''
 
-    total_hours = int(left_time) + int(left_duration)
-    total_minutes = int(right_time) + int(right_duration)
-    # global day, formatd
-    if total_hours > 12:
-        total_hours = total_hours - 12
-        if time[time_collen + 4:] == 'PM':
-            day = '(next day)'
-            formatd = 'AM'
-        else:
-            formatd = 'PM'
-            day = 'next day'
-        
+    if final_day:
+        result = f'{final_hour}:{str(final_minutes).zfill(2)} {period}, {final_day}{day_note}'
+    else:
+        result = f'{final_hour}:{str(final_minutes).zfill(2)} {period}{day_note}'
 
+    print(result)
+    return result
 
-    if total_minutes > 60:
-        remainder = total_minutes // 60
-        total_minutes = total_minutes - 60
-        total_hours += remainder
-        
-    print(f'{total_hours}:{total_minutes} {formatd} {day}')
-
-
-
-add_time('3:00 PM', '23:10')
